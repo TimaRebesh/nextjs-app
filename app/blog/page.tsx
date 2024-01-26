36bab7bd524cf6c1f43792d057785f4fde5c809b
Timothy <tsimafei.rebesh@external.mercell.com>
2024-01-26 12:53:09 +0100

    mongodb connections

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,10 +1,21 @@
-import React from 'react';
-import styles from './blog.module.css';
+import PostCard from "@/components/postCard/postCard";
+import styles from "./blog.module.css";
+import { getPosts } from "@lib/data";
 
-function Blog() {
+const BlogPage = async () => {
+
+  const posts = await getPosts();
+  console.log(posts);
+
   return (
-    <div>Blog</div>
+    <div className={styles.container}>
+      {/* {posts.map((post: any) => (
+        <div className={styles.post} key={post.id}>
+          <PostCard post={post} />
+        </div>
+      ))} */}
+    </div>
   );
-}
+};
 
-export default Blog;
+export default BlogPage;
