e02ac1abd7cf23b73945fe946bdef6efddce50f7
Timothy <tsimafei.rebesh@external.mercell.com>
2024-02-05 14:57:38 +0100

    admin page

--- lib/actions.tsx
+++ lib/actions.tsx
@@ -1,12 +1,84 @@
 'use server';
 import { signIn, signOut } from '@lib/auth';
 import { connectToDb } from './utils';
-import { User } from './models';
+import { Post, User } from './models';
 import bcrypt from "bcryptjs";
+import { revalidatePath } from "next/cache";
 
-export const sayHello = async () => {
-  console.log('hello');
-  return { some: 'hello ' };
+
+export const addPost = async (previousState: any, formData: FormData) => {
+  const { title, desc, slug, userId } = Object.fromEntries(formData);
+
+  try {
+    connectToDb();
+    const newPost = new Post({
+      title,
+      desc,
+      slug,
+      userId,
+    });
+
+    await newPost.save();
+    console.log("saved to db");
+    revalidatePath("/blog");
+    revalidatePath("/admin");
+  } catch (err) {
+    console.log(err);
+    return { error: "Something went wrong!" };
+  }
+};
+
+export const deletePost = async (formData: FormData) => {
+  const { id } = Object.fromEntries(formData);
+
+  try {
+    connectToDb();
+
+    await Post.findByIdAndDelete(id);
+    console.log("deleted from db");
+    revalidatePath("/blog");
+    revalidatePath("/admin");
+  } catch (err) {
+    console.log(err);
+    return { error: "Something went wrong!" };
+  }
+};
+
+export const addUser = async (previousState: any, formData: FormData) => {
+  const { username, email, password, img } = Object.fromEntries(formData);
+
+  try {
+    connectToDb();
+    const newUser = new User({
+      username,
+      email,
+      password,
+      img,
+    });
+
+    await newUser.save();
+    console.log("saved to db");
+    revalidatePath("/admin");
+  } catch (err) {
+    console.log(err);
+    return { error: "Something went wrong!" };
+  }
+};
+
+export const deleteUser = async (formData: FormData) => {
+  const { id } = Object.fromEntries(formData);
+
+  try {
+    connectToDb();
+
+    await Post.deleteMany({ userId: id });
+    await User.findByIdAndDelete(id);
+    console.log("deleted from db");
+    revalidatePath("/admin");
+  } catch (err) {
+    console.log(err);
+    return { error: "Something went wrong!" };
+  }
 };
 
 export const handleGitHubLogin = async () => {
